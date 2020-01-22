0343b9cb200ce597898f6acc442da330dcfc43b6
Juan Maquilon <juanmaquilon@Juans-MacBook-Pro.local>
2020-01-22 16:54:57 -0500

    Added form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import LoginForm from './loginForm';
 import './App.css';
 
 function App() {
   return (
     <div className='container'>
-      <div className='leftPanel'>Left Panel</div>
+      <div className='leftPanel'>
+        <LoginForm />
+      </div>
       <div className='rightPanel hide_on_narrow'>Right Panel</div>
     </div>
   );
